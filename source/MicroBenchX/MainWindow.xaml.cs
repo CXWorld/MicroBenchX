861935ee2cc55d3f74a24c3905fa93e24fc42178
Mark Fangmeyer <mark.fangmeyer@walter-machines.de>
2021-09-30 14:38:18 +0200

    lighweight workload

--- source/MicroBenchX/MainWindow.xaml.cs
+++ source/MicroBenchX/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -24,7 +25,7 @@ namespace MicroBenchX
     {
         public MainWindow()
         {
-            InitializeComponent();           
+            InitializeComponent();
         }
 
         // Lightweight workloads
@@ -36,48 +37,76 @@ namespace MicroBenchX
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            // Random memory access
+            //Task.Factory.StartNew(async () =>
+            //{
+            //    // 1GB: 1048576 
+            //    // 512MB: 524288
+            //    // 256MB: 262144
+            //    // 128MB: 131072
+            //    const int listSize = 524288 * 1024 / sizeof(double);
+            //    var dataArray = new double[listSize];
+
+            //    var gaussianRandom = new GaussianRandom();
+
+            //    for (int i = 0; i < listSize; i++)
+            //    {
+            //        dataArray[i] = gaussianRandom.GetNext();
+            //    }
+
+            //    bool doUpdateStatus = false;
+
+            //    // Create a timer with a two second interval.
+            //    var aTimer = new System.Timers.Timer(2000);
+            //    // Hook up the Elapsed event for the timer. 
+            //    aTimer.Elapsed += (o, e) => doUpdateStatus = true;
+            //    aTimer.AutoReset = true;
+            //    aTimer.Enabled = true;
+
+            //    do
+            //    {
+            //        var maxVal = DoRandomMemoryAccess(dataArray);
+            //        Debug.WriteLine($"Max value is {Math.Round(maxVal, 4)}");
+            //        await Task.Delay(1);
+
+            //        if (doUpdateStatus)
+            //        {
+            //            Debug.WriteLine($"Task is running on core #{GetCurrentProcessorNumber()}");
+            //            doUpdateStatus = false;
+            //        }
+
+            //    } while (true);
+            //});
+
+            // Random memory access
             Task.Factory.StartNew(async () =>
-              {
-                  // 1GB: 1048576 
-                  // 512MB: 524288
-                  // 256MB: 262144
-                  // 128MB: 131072
-                  const int listSize = 524288 * 1024 / sizeof(double);
-                  var dataArray = new double[listSize];
+            {
+                bool doUpdateStatus = false;
 
-                  var gaussianRandom = new GaussianRandom();
+                // Create a timer with a two second interval.
+                var aTimer = new System.Timers.Timer(2000);
+                // Hook up the Elapsed event for the timer. 
+                aTimer.Elapsed += (o, e) => doUpdateStatus = true;
+                aTimer.AutoReset = true;
+                aTimer.Enabled = true;
 
-                  for (int i = 0; i < listSize; i++)
-                  {
-                      dataArray[i] = gaussianRandom.GetNext();
-                  }
+                do
+                {
+                    var milliseconds = DoDeviceBandwidthTest();
+                    Debug.WriteLine($"{milliseconds} ms runtime");
+                    await Task.Delay(1);
 
-                  bool doUpdateStatus = false;
+                    if (doUpdateStatus)
+                    {
+                        Debug.WriteLine($"Task is running on core #{GetCurrentProcessorNumber()}");
+                        doUpdateStatus = false;
+                    }
 
-                  // Create a timer with a two second interval.
-                  var aTimer = new System.Timers.Timer(2000);
-                  // Hook up the Elapsed event for the timer. 
-                  aTimer.Elapsed += (o, e) => doUpdateStatus = true;
-                  aTimer.AutoReset = true;
-                  aTimer.Enabled = true;
-
-                  do
-                  {
-                      DoRandomMemoryAccess(dataArray);
-                      await Task.Delay(1);
-                      
-                      if (doUpdateStatus)
-                      {
-                          Debug.WriteLine($"Task is running on core #{GetCurrentProcessorNumber()}");
-                          doUpdateStatus = false;
-                      }
-
-                  } while (true);
-              });
-
+                } while (true);
+            });
         }
 
-        private void DoRandomMemoryAccess(double[] dataArray)
+        private double DoRandomMemoryAccess(double[] dataArray)
         {
             int listSize = dataArray.Length;
             var randomIndex = new Random();
@@ -101,7 +130,56 @@ namespace MicroBenchX
                     maxVal = curVal;
             }
 
-            Debug.WriteLine($"Max value is {Math.Round(maxVal, 4)}");
+            return maxVal;
+        }
+
+        private long DoDeviceBandwidthTest()
+        {
+            int testIterations = 100;
+            int appendIterations = 40;
+            var stopwatch = new Stopwatch();
+            string randomText = GetRandomString(100000);
+
+            if (!Directory.Exists(@"C:\Temp"))
+                Directory.CreateDirectory(@"C:\Temp");
+
+            stopwatch.Start();
+            for (int j = 1; j <= testIterations; j++)
+            {
+                var path = $@"C:\Temp\{j}_test.tmp";
+                var pathCopy = $@"C:\Temp\{j}_testCopy.tmp";
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+                StreamWriter sWriter = new StreamWriter(path, true);
+
+                for (int i = 1; i <= appendIterations; i++)
+                {
+                    sWriter.Write(randomText);
+                }
+                sWriter.Close();
+
+                File.Copy(path, pathCopy);              
+                File.Delete(path);
+                File.Delete(pathCopy);
+            }
+            stopwatch.Stop();
+
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        private string GetRandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                builder.Append(Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65))));
+            }
+            return builder.ToString();
         }
     }
 }
